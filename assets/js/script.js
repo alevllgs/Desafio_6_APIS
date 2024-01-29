2697ef03cef73e941a0ec4779c7eeb3bd56ea9f2
Alejandro Villegas <alejandro.villegas.m@gmail.com>
2024-01-28 22:44:42 -0300

    up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,25 @@
-console.log("hola")
-async function api(){
-    try{
-        const response = await fetch('https://mindicador.cl/api/')
-        const data = await response.json()
-        console.log(data.uf.valor)
-    } catch{
-        alert("error")
+function buscarValor() {
+    var monedaSeleccionada = document.getElementById("listaDesplegable").value;
+    var montoCLP = parseFloat(document.getElementById("campoTexto").value);
+
+    if (isNaN(montoCLP)) {
+        alert("Por favor, ingrese un monto válido en CLP.");
+        return;
     }
+
+    fetch('https://mindicador.cl/api/')
+        .then(response => response.json())
+        .then(data => {
+            var valorMoneda = data[monedaSeleccionada].valor;
+
+            if (isNaN(valorMoneda)) {
+                alert("No se pudo obtener el valor de la moneda desde la API.");
+                return;
+            }
+
+            var resultado = montoCLP * valorMoneda;
+
+            document.getElementById("resultado").innerHTML = "Resultado: " + resultado.toFixed(2) + " " + monedaSeleccionada;
+        })
+        .catch(error => console.error('Error al obtener datos de la API', error));
 }
-api()
-console.log("chau") 
