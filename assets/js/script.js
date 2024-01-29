a4779447d842eb05e7ab4edc263f06da511d093b
Alejandro Villegas <alejandro.villegas.m@gmail.com>
2024-01-29 09:57:25 -0300

    corrige errores de conversión y de JS en HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
+
+
+
 function buscarValor() {
-    let monedaSeleccionada = document.getElementById("listaDesplegable").value
-    let montoCLP = parseFloat(document.getElementById("campoTexto").value)
+    // Obtener el valor del campo de texto y la opción seleccionada
+    var montoCLP = document.getElementById('campoTexto').value;
+    var monedaSeleccionada = document.getElementById('listaDesplegable').value;
+
+    // Realizar acciones con los valores obtenidos (puedes modificar esto según tus necesidades)
+    console.log('Monto en CLP:', montoCLP);
+    console.log('Moneda seleccionada:', monedaSeleccionada);
 
     if (isNaN(montoCLP)) {
         alert("Por favor, ingrese un monto válido en CLP.")
@@ -13,9 +21,18 @@ function buscarValor() {
             let valorMoneda = data[monedaSeleccionada].valor
 
 
-            let resultado = montoCLP * valorMoneda
+            let resultado = montoCLP / valorMoneda
 
             document.getElementById("resultado").innerHTML = "Resultado: " + resultado.toFixed(2) + " " + monedaSeleccionada
         })
         .catch(error => console.error('Error al obtener datos de la API', error))
 }
+
+// Asignar la función al evento submit del formulario
+document.getElementById('miFormulario').addEventListener('submit', function(event) {
+    // Evitar el envío del formulario
+    event.preventDefault();
+
+    // Llamar a la función buscarValor
+    buscarValor();
+});
